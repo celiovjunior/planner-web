7e6b528ad79a482c887a62fddc06232e96901f1d
Celio Junior <celio_cjr@hotmail.com>
2024-10-09 20:31:03 +0930

    feat: finish the homepage (App file).

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import { Calendar, MapPin } from "lucide-react";
+import { Calendar, MapPin, ArrowRight } from "lucide-react";
 
 export function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
 
-        <p className="text-zinc-300 text-lg">Invite your friends and plan your next trip!</p>
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="plan.er application logo" />
+          <p className="text-zinc-300 text-lg">Invite your friends and plan your next trip!</p>
+        </div>
+
         
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
           <div className="flex items-center gap-2">
             <MapPin className="size-5 text-zinc-400" />
-            <input type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400" />
+            <input type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
           </div>
 
           <div className="flex items-center gap-2">
             <Calendar className="size-5 text-zinc-400" />
-            <input type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400" />
+            <input type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400 outline-none" />
           </div>
-          <button>Continue</button>
+
+          <div className="w-px h-6 bg-zinc-800" />
+
+          <button className="flex items-center gap-2 bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-400">
+            Continue
+            <ArrowRight className="size-7" />
+          </button>
         </div>
 
-        <p className="text-sm text-zinc-500">When planning your travels with Plann.er, you automatically agree<br />to follow our <a className="text-zinc-300 underline" href="#">rules and privacy policies</a>.</p>
+        <p className="text-sm text-zinc-500">
+          When planning your travels with Plann.er, you automatically agree<br />to follow our <a className="text-zinc-300 underline" href="#">rules and privacy policies</a>.
+        </p>
       </div>
     </div>
   )
